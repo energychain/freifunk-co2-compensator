c0aca291d6bcd948bd3dae9a1d69c7cdbdaac96b
Thorsten Zoerner <me@thorsten-zoerner.com>
2019-12-16 01:43:48 +0100

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # freifunk-co2-compensator
 Calculator service for carbon footprint of Freifunk Rhein Neckar nodes. Demo for Corrently IoT proof of sustainability in complex and dynamic infrastructure environements (CDIEs).
+
+## Purpose of this script
+This script is intended to be used on a command line to run a CO2 footprint calculation of Freifunk-Rhein-Neckar active nodes. It is a demonstrator to illustrate usage of Corrently API as a backbone for a sustainability strategy.
+
+## Installation Node JS
+```
+npm install -g freifunk-co2-compensator
+```
+
+## Execute / Run
+```
+freifunk-co2-compensator
+```
+
+## Sample Output
+```
+Meter Id freifunk_rrnk1576430805760
+Current Power Load (kW): 4.523
+Total Consumption/Meter Reading (kWh): 30.996
+Immutable URL of virtual meter reading: https://api.corrently.io/core/reading?account=0xE4C6a242633148634BD7205D742e6f14B30d95BB
+Immutable URL for CO2 comission of virtual meter https://api.corrently.io/core/emission?account=0xE4C6a242633148634BD7205D742e6f14B30d95BB
+Total CO2 emission (kg) 9.926
+Compensated CO2 (kg) 0
+Waiting for Compensation (kg) 10.457
+```
+
+The two URLs will be individual per instance (e.q. server, installation or path executed) and will not change. By executing this script via a cron those urls might be used to
+show CO2 footprint and compensation.
+
+## Further Reading
+- https://corrently.io/  - API used in this demonstration
+- https://freifunk-rhein-neckar.de/ - Freifunk Rhein-Neckar homepage
+- https://corrently.de/co2.html - CO2 Compensation for power usage (Germany)
+- https://gruenstromindex.de/ - GrünstromIndex (GreenPowerIndex) - used in the background for CO2 Calculation of consumed energy
+- https://stromdao.de/ - Maintainer of this Script
---
+++ index.js
@@ -0,0 +1,125 @@
+#!/usr/bin/env node
+
+const http_request = require("request");
+const storage = require('node-persist');
+
+const nodeListURL = "https://www.freifunk-rhein-neckar.de/proxy.php"; // URL where we gett the current nodes status
+const ZIP = "69256"; // German Postleitzahl to use for localization of GrünstromIndex
+
+const co2_grey = 0.482;     // gramm co2 per Wh (standard)
+const co2_green = 0.035;     // gramm co2 per Wh (green energy)
+
+/**
+ * Use Freifunk Map Endpoint to get a list of current online nodes
+ */
+const retrieveCurrentyNodeList = function() {
+  return new Promise(async function (resolve, reject)  {
+      http_request(nodeListURL,function(e,r,b) {
+        try {
+          let json = JSON.parse(b);
+          resolve(json.nodes);
+        } catch(e) {
+          reject("Unable to retrieve and parse nodelist from url "+nodeListUrl+" "+e);
+        }
+      });
+  });
+}
+
+/**
+ * Calculates the current Power (Watt) - we use this as base to calculate Consumption (Watthours)
+ *  - Iterate the node list and add active nodes
+ *  - Each active node we put a base power of 5 Watt
+ *  - For each connected user we add another Watt (handling)
+ *
+ *  In future releases we might destinguish between different hardware - or do some other nice stuff
+ **/
+const calculateCurrentPower = function(active_nodes) {
+  return new Promise(async function (resolve, reject)  {
+        let total_power = 0;
+        for(let i=0; i<active_nodes.length ; i++) {
+          if(active_nodes[i].is_online == true) {
+            total_power += 5; // 5 Watt as base power usage
+            total_power += (active_nodes[i].clients * 1) // 1 Watt per Client connected
+          }
+        }
+        resolve(total_power); // resolves the total actual power in Watt
+  });
+}
+
+/**
+ * Use Corrently API to create/maintain a virtual meter for this instance
+ * We just need to push the latest power and the API endpoint will care about
+ * all calculations in the background.
+ * To avoid a key handling we use a generated meterid + secret to confirm updates
+ **/
+const storeConsumption = function(current_power,meterid,metersecret) {
+  return new Promise(async function (resolve, reject)  {
+      let readingdata = {
+        power:current_power,
+        zip:ZIP,
+        externalAccount:meterid,
+        secret:metersecret
+      };
+
+      http_request.post("https://api.corrently.io/core/reading",{form:readingdata},function(e,r,b) {
+        try {
+          let json = JSON.parse(b);
+          resolve(json);
+        } catch(e) {
+          reject("Unable to store reading "+e);
+        }
+      });
+  });
+}
+
+/**
+ * Trigger Corrently API update of CO2 emission calculation
+ **/
+const calculateCO2 = function(account) {
+  return new Promise(async function (resolve, reject)  {
+      // We let Corrently API handle the CO2 Calculation
+      http_request.get("https://api.corrently.io/core/emission?account="+account,function(e,r,b) {
+        try {
+          let json = JSON.parse(b);
+          resolve(json);
+        } catch(e) {
+          reject("Unable to retrieve co2 calculation "+e);
+        }
+      });
+  });
+}
+
+const main = function() {
+  return new Promise(async function (resolve, reject)  {
+
+    // Manage local meter instance
+    await storage.init();
+    let meterid = await storage.getItem("meterid");
+    let metersecret = await storage.getItem("metersecret");
+    if(typeof meterid == "undefined") {                   //if we do not have a meterid for this instance - we create a new one and persist it localy
+      meterid = "freifunk_rrnk"+new Date().getTime();
+      await storage.setItem("meterid",meterid);
+      metersecret = Math.random()*new Date().getTime();   // this is not the best random secret generator, but should do for this purpose
+      await storage.setItem("metersecret",metersecret);
+    }
+    console.log("Meter Id",meterid);
+
+    // Calculation of Energy Consumption
+    let current_power = await calculateCurrentPower(await retrieveCurrentyNodeList());
+    console.log("Current Power Load (kW):",(current_power/1000));
+
+    // Blockchain Operation for Virtual Meter
+    let oracle = await storeConsumption(current_power,meterid,metersecret);   // Use Corrently Blockchain Oracle to store and persist virtual Meter reading
+    console.log("Total Consumption/Meter Reading (kWh):",oracle["1.8.0"]/1000);
+    console.log("Immutable URL of virtual meter reading:","https://api.corrently.io/core/reading?account="+oracle.account);
+    console.log("Immutable URL for CO2 comission of virtual meter","https://api.corrently.io/core/emission?account="+oracle.account)
+    // Do CO2 Footprint Calculation
+    let co2calculation = await calculateCO2(oracle.account);
+    console.log("Total CO2 emission (kg)",(co2calculation.co2/1000));
+    console.log("Compensated CO2 (kg)",co2calculation.compensation.base_haben/1000);
+    console.log("Waiting for Compensation (kg)",(co2calculation.compensation.balance_base/-1000));
+    resolve();
+  });
+}
+
+main();
